390e215af0f2f002f577deacdbe667e4bae8e734
Smriti Kshatriya <kbtug23384@kbtcoe.org>
2026-04-26 18:28:52 +0530

    A6

---
+++ A6/node-static-app/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve static files from 'public' folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Start server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
